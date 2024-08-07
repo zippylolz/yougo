a9c961d116e320002b436860b9b46d61ced56608
zippylolz <zach_migliozzi@ca6.uscourts.gov>
2024-08-07 15:31:55 -0400

    wip

--- routes/routes.go
+++ routes/routes.go
@@ -11,19 +11,12 @@ import (
 func RegisterRoutes(app *fiber.App) {
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		comp := components.Home()
-		return utils.Render(c, comp)
+		return utils.Render(c, components.Home())
 	})
-
-	// app.Get("/hello", func(c *fiber.Ctx) error {
-	// 	name := c.Query("name", "Randy")
-	// 	return utils.Render(c, components.Hello(name))
-	// })
 
 	app.Get("/videos/:v", func(c *fiber.Ctx) error {
 		video := c.Params("v", "12345")
 		msg := fmt.Sprintf("Page for video %s.", video)
 		return c.SendString(msg)
 	})
-
 }
